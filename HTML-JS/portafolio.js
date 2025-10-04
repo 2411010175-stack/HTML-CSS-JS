d29fffc6a581012d1da1de2151059250a8050dba
Alex <2411010175@undc.edu.pe>
2025-10-04 18:43:19 -0500

    finalConElLogin

--- HTML-JS/portafolio.js
+++ HTML-JS/portafolio.js
@@ -1,8 +1,12 @@
 // Seleccionamos el botón y la barra lateral
 const toggle = document.getElementById("menu-toggle");
+const subtitulo = document.getElementById("Subtitulossss");
 const barraLateral = document.querySelector(".barra-lateral");
 
 // Evento: abrir/cerrar barra lateral
 toggle.addEventListener("click", () => {
+    barraLateral.classList.toggle("active");
+});
+subtitulo.addEventListener("click", () => {
     barraLateral.classList.toggle("active");
 });
